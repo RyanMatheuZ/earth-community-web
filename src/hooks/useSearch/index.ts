7320b7de49d1cc2d4ef7c3ff0663d8873c5e15da
RyanMatheuZ <contato.ryanoliveira@outlook.com>
2023-06-16 22:27:28 -0300

    feat: search

---
+++ src/hooks/useSearch/index.ts
@@ -0,0 +1,41 @@
+import { useCallback } from 'react';
+
+import { type AxiosResponse } from 'axios';
+
+import { useQuery } from '@tanstack/react-query';
+
+import axiosInstance from '@services/axios';
+
+import { catchError } from '@utils/requestMessages';
+
+import { type SearchResults } from './utils';
+
+const useSearch = () => {
+  const handleSearch = useCallback((name: string) => {
+    const ENDPOINT = '/search';
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    return useQuery(
+      ['search', name],
+      async () => {
+        try {
+          axiosInstance.interceptors.response.clear();
+
+          const { data }: AxiosResponse<{ result: SearchResults }> = await axiosInstance.get(
+            `${ENDPOINT}?name=${name}`
+          );
+
+          return data.result;
+        } catch (e) {
+          catchError(e);
+        }
+      }
+    );
+  }, []);
+
+  return {
+    handleSearch
+  };
+};
+
+export default useSearch;
